dd632bdda2043e53b7e3e604b94b4e90e0ebc6fa
PepeLasagna <tiagoapkp@gmail.com>
2023-08-23 20:11:40 -0300

    psuhea rapido pushea

--- js/almacenar.js
+++ js/almacenar.js
@@ -4,13 +4,31 @@ btn.addEventListener("click", () => {
 
 let item = document.getElementById("item").value;
 let data = JSON.parse(localStorage.getItem("lista")) || [];
-data.unshift(item);
+data.push(item);
 
 localStorage.setItem("lista", JSON.stringify(data));
 let container = document.getElementById("contenedor");
-container.innerHTML += `<div>${data[0]}<br></div>`;
+let index = data.length -1
+container.innerHTML += `<div>${data[index]}<br></div>`;
 document.getElementById("item").value = "";
 })
 
 
+let remove = document.getElementById("limpiar")
 
+remove.addEventListener("click", () => {
+localStorage.removeItem("lista");
+let container = document.getElementById("contenedor");
+container.innerHTML = ""
+
+
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    let container = document.getElementById("contenedor");
+    let data = JSON.parse(localStorage.getItem("lista")) || [];
+   
+    data.forEach(element => {
+        container.innerHTML += `<div>${element}<br></div>`
+    });
+})
